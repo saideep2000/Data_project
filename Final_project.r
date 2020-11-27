e295df4c082c55b5f76e3d7631a6c56ffd5d8c6b
saideep2000 <73835168+saideep2000@users.noreply.github.com>
2020-11-27 19:57:54 +0530

    Add files via upload

---
+++ Final_project.r
@@ -0,0 +1,14 @@
+library(tidyverse) # general
+library(ggalt) # dumbbell plots
+library(countrycode) # continent
+library(rworldmap) # quick country-level heat maps
+library(gridExtra) # plots
+library(broom) # significant trends within countries
+
+data <- read_csv("~/Windows_desktop/4th sem1/Data/master.csv")
+view(data)
+
+Rem=data
+view(Rem)
+
+
